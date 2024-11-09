77b2052d88a8b82f0a83652bac98437991a175a0
Progresst-8 <142147661+Progresst-8@users.noreply.github.com>
2024-11-09 22:12:00 +0800

    Add tracker for category limits

--- src/main/java/wheresmymoney/category/CategoryLimitTracker.java
+++ src/main/java/wheresmymoney/category/CategoryLimitTracker.java
@@ -2,24 +2,44 @@ package wheresmymoney.category;
 
 import java.util.HashMap;
 
-public class CategorySpendingLimit {
+import wheresmymoney.exception.WheresMyMoneyException;
+
+public class CategoryLimitTracker {
+    public static final Float DEFAULT_LIMIT = 100F;
     private HashMap<String, Float> categoryLimits;
     
-    public CategorySpendingLimit() {
+    public CategoryLimitTracker() {
         categoryLimits = new HashMap<>();
     }
-    
+    public HashMap<String, Float> getLimits() {
+        return categoryLimits;
+    }
     public boolean isEmpty() {
         return categoryLimits.isEmpty();
     }
     public int size() {
         return categoryLimits.size();
     }
+    public boolean contains(String category) {
+        return categoryLimits.containsKey(category);
+    }
     
     public Float getLimitFor(String category) {
+        if (!categoryLimits.containsKey(category)) {
+            throw new WheresMyMoneyException("Limit cannot be found as no such category exists.");
+        }
         return categoryLimits.get(category);
     }
-    public void setLimitFor(String category, Float spendingLimit) {
+    public void setDefaultLimitFor(String category) {
+        categoryLimits.put(category, DEFAULT_LIMIT);
+    }
+    public void setCustomLimitFor(String category, Float spendingLimit) {
+        if (spendingLimit < 0) {
+            throw new WheresMyMoneyException("Limit should not be negative.");
+        }
         categoryLimits.put(category, spendingLimit);
+    }
+    public void removeLimitFor(String category) {
+        categoryLimits.remove(category);
     }
 }
