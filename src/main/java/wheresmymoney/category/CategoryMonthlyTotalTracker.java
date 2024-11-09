6c9a1ec8ab5fb251cbc195ec8846ea837090021d
Progresst-8 <142147661+Progresst-8@users.noreply.github.com>
2024-11-09 22:14:03 +0800

    Update class that splits category totals by month

--- src/main/java/wheresmymoney/category/CategoryMonthlyTotalTracker.java
+++ src/main/java/wheresmymoney/category/CategoryMonthlyTotalTracker.java
@@ -9,12 +9,10 @@ import wheresmymoney.exception.WheresMyMoneyException;
 /**
  * Sorts CategoryTracker by months
  */
-public class CategoryManager {
+public class CategoryMonthlyTotalTracker {
     private HashMap<LocalDate, CategoryTotalTracker> monthlyCategoryTotals;
     
-    // wrapper to Facade?
-    
-    public CategoryManager() {
+    public CategoryMonthlyTotalTracker() {
         this.monthlyCategoryTotals = new HashMap<>();
     }
     public HashMap<LocalDate, CategoryTotalTracker> getMonthlyCategoryTotals() {
@@ -26,53 +24,53 @@ public class CategoryManager {
     public boolean isEmpty() {
         return monthlyCategoryTotals.isEmpty();
     }
-    private boolean containsCategoryTotalsInPeriod(LocalDate localDate) {
+    public boolean containsTotalsForPeriodOf(LocalDate localDate) {
         LocalDate yearMonth = DateUtils.dayMonthYearToYearMonth(localDate);
         return monthlyCategoryTotals.containsKey(yearMonth);
     }
-    public void checkLimitOf(String category) throws WheresMyMoneyException {
-        LocalDate currYearMonth = DateUtils.dayMonthYearToYearMonth(DateUtils.getCurrentDate());
-        CategoryTotalTracker categoryTotalTracker = getTrackerOfPeriod(currYearMonth);
-        categoryTotalTracker.checkLimitOf(category);
-    }
     
-    private CategoryTotalTracker getTrackerOfPeriod(LocalDate localDate) throws WheresMyMoneyException {
-        LocalDate yearMonth = DateUtils.dayMonthYearToYearMonth(localDate);
-        if (containsCategoryTotalsInPeriod(yearMonth)) {
+    private CategoryTotalTracker getTrackerForPeriodOf(LocalDate localDate) throws WheresMyMoneyException {
+        LocalDate yearMonth = DateUtils.dayMonthYearToYearMonth(localDate); // convert just in case
+
+        if (containsTotalsForPeriodOf(yearMonth)) {
             throw new WheresMyMoneyException("There is no categories tracked for the period of" +
                     yearMonth.getMonth() + " " + yearMonth.getYear());
         }
         return monthlyCategoryTotals.get(yearMonth);
     }
     
-    public void insertIntoTracker(LocalDate localDate, String category, Float price) throws WheresMyMoneyException {
+    public void increaseTotalsOf(LocalDate localDate, String category, Float price) throws WheresMyMoneyException {
         LocalDate yearMonth = DateUtils.dayMonthYearToYearMonth(localDate);
-        if (!this.containsCategoryTotalsInPeriod(yearMonth)) {
+
+        if (!this.containsTotalsForPeriodOf(yearMonth)) {
             monthlyCategoryTotals.put(yearMonth, new CategoryTotalTracker());
         }
-        CategoryTotalTracker categoryTotalTracker = this.getTrackerOfPeriod(yearMonth);
-        categoryTotalTracker.addCategory(category, price);
+        CategoryTotalTracker categoryTotalTracker = getTrackerForPeriodOf(yearMonth);
+        categoryTotalTracker.increaseTotalBy(category, price);
     }
-    public void removeFromTracker(LocalDate localDate, String category, Float price) throws WheresMyMoneyException {
+    public void decreaseTotalsOf(LocalDate localDate, String category, Float price) throws WheresMyMoneyException {
         LocalDate yearMonth = DateUtils.dayMonthYearToYearMonth(localDate);
-        CategoryTotalTracker categoryTotalTracker = this.getTrackerOfPeriod(yearMonth);
-        categoryTotalTracker.deleteCategory(category, price);
+
+        CategoryTotalTracker categoryTotalTracker = getTrackerForPeriodOf(yearMonth);
+        categoryTotalTracker.decreaseTotalBy(category, price);
         if (categoryTotalTracker.isEmpty()) {
-            monthlyCategoryTotals.remove(localDate);
+            monthlyCategoryTotals.remove(yearMonth);
         }
     }
-    public void editInTracker(LocalDate oldDate, LocalDate newDate, String oldCategory, String newCategory,
-                              Float oldPrice, Float newPrice) throws WheresMyMoneyException {
+    public void editTotalsOf(LocalDate oldDate, LocalDate newDate, String oldCategory, String newCategory,
+                             Float oldPrice, Float newPrice) throws WheresMyMoneyException {
         LocalDate oldYearMonth = DateUtils.dayMonthYearToYearMonth(oldDate);
         LocalDate newYearMonth = DateUtils.dayMonthYearToYearMonth(newDate);
-        removeFromTracker(oldYearMonth, oldCategory, oldPrice);
-        insertIntoTracker(newYearMonth, newCategory, newPrice);
+
+        decreaseTotalsOf(oldYearMonth, oldCategory, oldPrice);
+        increaseTotalsOf(newYearMonth, newCategory, newPrice);
     }
     
-    public void setSpendingLimitFor(String category, Float spendingLimit) {
+    public Float getCurrMonthTotalFor(String category) throws WheresMyMoneyException {
         LocalDate currYearMonth = DateUtils.dayMonthYearToYearMonth(DateUtils.getCurrentDate());
-        CategoryTotalTracker categoryTotalTracker = getTrackerOfPeriod(currYearMonth);
-        categoryTotalTracker.setSpendingLimitFor(category, spendingLimit);
+
+        CategoryTotalTracker categoryTotalTracker = getTrackerForPeriodOf(currYearMonth);
+        return categoryTotalTracker.getTotalFor(category);
     }
     
 }
